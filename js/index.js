78652b6b50186ef2844d4914e5857057a1b3d3b3
Mathias Latronico <mathiezelat@gmail.com>
2021-05-19 14:22:07 -0300

    CDV - v8.0.0

--- js/index.js
+++ js/index.js
@@ -7,11 +7,11 @@ subrayadoAnimacion('notas');
 const subrayadoAnimacion = (valor)=>{
     let animacion = document.getElementById(`subrayado-color-${valor}`)
     let position = animacion.getBoundingClientRect().top;
-    console.log(animacion)
-    console.log(position)
     let screen = window.innerHeight/1.5;
     const root = document.querySelector(":root");
     if(position<screen){
         root.style.setProperty(`--animacion-${valor}`, 'subrayado 2s ease both');
+    } else{
+        root.style.setProperty(`--animacion-${valor}`, 'subrayado-reverse 2s ease both')
     }
 }
